c893e60eb61c25db01474b2387f0c545a4de5e5a
Jan Juracka <xjurack1@fi.muni.cz>
2022-11-26 19:47:53 +0100

    convenience improvements

--- create_pywrap.sh
+++ create_pywrap.sh
@@ -1,3 +1,21 @@
 #! /bin/bash
 
-cat src/standalones/*/wrapper.py > $1
+echo "import numpy as np
+import subprocess
+from skimage import io
+import os
+
+
+def _call(name: str, img: np.ndarray, *args) -> np.ndarray:
+    INP_NAME = '__inp.tif'
+    OUT_NAME = '__out.tif'
+    io.imsave(INP_NAME, img, check_contrast=False, compression='zlib')
+    subprocess.run([name, INP_NAME, OUT_NAME, *args], check=True)
+    img = io.imread(OUT_NAME)
+    os.remove(INP_NAME)
+    os.remove(OUT_NAME)
+    return img
+    
+" > $1 
+
+cat src/standalones/*/wrapper.py >> $1
