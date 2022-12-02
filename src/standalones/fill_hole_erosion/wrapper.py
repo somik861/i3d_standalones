6ab21403285b37688d03c52e993d49a5bb223b9f
Jan Juracka <xjurack1@fi.muni.cz>
2022-12-02 18:41:03 +0100

    improve naming

--- src/standalones/fill_hole_erosion/wrapper.py
+++
@@ -1,2 +0,0 @@
-def fill_hole_erosion(img: np.ndarray) -> np.ndarray:
-    return _call('fill_hole_erosion', img)
---
+++ src/standalones/fill_white_holes/wrapper.py
@@ -0,0 +1,2 @@
+def fill_white_holes(img: np.ndarray) -> np.ndarray:
+    return _call('fill_white_holes', img)
