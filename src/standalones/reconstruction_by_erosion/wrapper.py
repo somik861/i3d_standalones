a429937480bde5939d4694e98887d12cb4335a01
Jan Juracka <xjurack1@fi.muni.cz>
2022-12-03 10:30:43 +0100

    added validity check to wrapper

--- src/standalones/reconstruction_by_erosion/wrapper.py
+++ src/standalones/reconstruction_by_erosion/wrapper.py
@@ -1,6 +1,6 @@
 def reconstruction_by_erosion(img: np.ndarray, marker: np.ndarray) -> np.ndarray:
     assert np.all(marker >= img)
-    io.imsave('__marker.tif', marker)
+    io.imsave('__marker.tif', marker, check_contrast=True)
     rv = _call('reconstruction_by_erosion', img, '__marker.tif')
     os.remove('__marker.tif')
     return rv
