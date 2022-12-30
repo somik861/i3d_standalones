faf28c6117e2ed3cd25fe3ba7fbc117e8c2ad43c
Jan Juracka <xjurack1@fi.muni.cz>
2022-12-30 13:56:06 +0100

    fixed wrapper

--- create_pywrap.sh
+++ create_pywrap.sh
@@ -7,10 +7,11 @@ import os
 
 
 def _call(name: str, img: np.ndarray, *args) -> np.ndarray:
+    str_args = tuple(str(arg) for arg in args)
     INP_NAME = '__inp.tif'
     OUT_NAME = '__out.tif'
     io.imsave(INP_NAME, img, check_contrast=False, compression='zlib')
-    subprocess.run([os.path.join(os.path.dirname(os.path.realpath(__file__)), 'bin', name), INP_NAME, OUT_NAME, *args], check=True)
+    subprocess.run([os.path.join(os.path.dirname(os.path.realpath(__file__)), 'bin', name), INP_NAME, OUT_NAME, *str_args], check=True)
     img = io.imread(OUT_NAME)
     os.remove(INP_NAME)
     os.remove(OUT_NAME)
