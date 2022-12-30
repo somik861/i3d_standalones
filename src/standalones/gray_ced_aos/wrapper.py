c1d50b121cc7401c1ddf92865fec661c1df94515
Jan Juracka <xjurack1@fi.muni.cz>
2022-12-30 13:38:33 +0100

    fixed wrapper

--- src/standalones/gray_ced_aos/wrapper.py
+++ src/standalones/gray_ced_aos/wrapper.py
@@ -1,2 +1,2 @@
-def load_save(img: np.ndarray, sigma: float, rho: float, tau: float, num_iter: int) -> np.ndarray:
+def gray_ced_aos(img: np.ndarray, sigma: float, rho: float, tau: float, num_iter: int) -> np.ndarray:
     return _call('gray_ced_aos', img, sigma, rho, tau, num_iter)
--- src/standalones/gray_eed_aos/wrapper.py
+++ src/standalones/gray_eed_aos/wrapper.py
@@ -1,2 +1,2 @@
-def load_save(img: np.ndarray, sigma: float, lambda_: float, tau: float, num_iter: int) -> np.ndarray:
-    return _call('gray_eed_aos', img, sigma, rho, lambda_, num_iter)
+def gray_eed_aos(img: np.ndarray, sigma: float, lambda_: float, tau: float, num_iter: int) -> np.ndarray:
+    return _call('gray_eed_aos', img, sigma, lambda_, tau, num_iter)
